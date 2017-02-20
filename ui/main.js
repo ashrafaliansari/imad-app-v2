7bb936e596f9308c5a8a4b455232cab2dd2cc559
ashrafaliansari <ali.ansariashraf0341@gmail.com>
2017-02-20 12:56:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@
 var button=document.getElementById('counter');
 var count=0;
 //var counter=0;
-button.onlick= function()
+button.onclick= function()
 {   
     count=count+1;
     var span=document.getElementById('counting');
