a5ff9c29873a44c16f5f948c468cb9998fa25bd5
ashrafaliansari <ali.ansariashraf0341@gmail.com>
2017-02-20 12:32:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //console.log('Loaded!');
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onlick= function()
 {
     //create a requst object  for the  counter end point 
@@ -15,7 +15,7 @@ button.onlick= function()
             {
                 var counter=request.responseText;
                 var span=document.getElementById('count');
-                span.innerHTML=counter.toStrong();
+                span.innerHTML=counter.toString();
             }
         
         }
