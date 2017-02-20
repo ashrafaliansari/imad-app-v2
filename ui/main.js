eb09f0e4a8be5b0fca233285978f78c3af85bea3
ashrafaliansari <ali.ansariashraf0341@gmail.com>
2017-02-20 12:21:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
 console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+button.onlick= function()
+{
+    //create a requst object  for the  counter end point 
+    var request=new XMLHttpRequest();
+    //capture the respoonse and store it in avariable
+    reuest.onreadystatechange=function()
+    {
+        if(request.readystate===XMLRequest.DONE)
+        {//take some action
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toStrong();
+            }
+        
+        }
+    };
+};
