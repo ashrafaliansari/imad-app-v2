e42d52aa5c2f69ce0c129bd0d468270f2046c58d
ashrafaliansari <ali.ansariashraf0341@gmail.com>
2017-02-20 12:45:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,11 @@
 //console.log('Loaded!');
 //counter code
 var button=document.getElementById('counter');
+var counter=0;
 //var counter=0;
 button.onlick= function()
-{
-    //create a requst object  for the  counter end point 
-    var request=new XMLHttpRequest();
-    //capture the respoonse and store it in avariable
-    reuest.onreadystatechange=function()
-    {
-        if(request.readystate === XMLRequest.DONE)
-        {//take some action
-            if(request.status===200)
-            {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
-            }
-        
-        }
-    };
-
-//make a request
-request.open('GET','http://ashrafaliansari.imad.hasura-app.io/counter', true);
-request.send(null);
+{   
+    counter++;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
